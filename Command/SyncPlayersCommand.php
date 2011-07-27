e5a4c27a41545ae10cf021e29bce7f69e9e9c952
Eric Barr <eb@anhstudios.com>
2011-07-27 08:31:53 -0400

    Added a command that will generate player accounts from an AnhGalaxyManagerBundle instance. In the future we can provide support for more sources (such as common php forums). This command can be run standalone or set up as a cron to be run regularly.

---
+++ Command/SyncPlayersCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Anh\SwgManagerBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Anh\SwgManagerBundle\Entity\Player;
+
+/**
+ * @note: later on we can add flags to sync with multiple types of sources.
+ * For now we are just syncing with an AnhGalaxyManagerBundle instance.
+ */
+class SyncPlayersCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('swg:players:sync')
+            ->setDescription('Sync player table with the master galaxy manager instance')
+            ->addArgument('maxAccounts', InputArgument::OPTIONAL, 'How many characters to allow?')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $maxAccounts = $input->getArgument('maxAccounts');
+        if (!$maxAccounts) {
+            $maxAccounts = 4;
+        }
+
+        $doctrine = $this->getContainer()->get('doctrine');
+
+        $accounts = $doctrine->getRepository('AnhGalaxyManagerBundle:Account')
+                             ->findAll();
+
+        foreach($accounts as $account) {
+            $player = $doctrine->getRepository('AnhSwgManagerBundle:Player')
+                             ->findOneByReferenceId($account->getId());
+
+            if (!$player) {
+                $output->writeln('<info>Adding player entry for user: '. $account->getUsername() .'</info>');
+
+                $player = new Player();
+                $player->setReferenceId($account->getId());
+                $player->setMaxCharacters($maxAccounts);
+
+                $em = $doctrine->getEntityManager();
+                $em->persist($player);
+                $em->flush();
+            }
+        }
+    }
+}
