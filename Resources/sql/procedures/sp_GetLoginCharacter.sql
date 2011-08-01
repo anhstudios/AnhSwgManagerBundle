21718ab1c74b5bf1e2385a435edc69f0e8f9a970
Kyle Craviotto <kyle@anhstudios.com>
2011-07-31 19:41:18 -0700

    remove definer line

--- Resources/sql/procedures/sp_GetLoginCharacter.sql
+++ Resources/sql/procedures/sp_GetLoginCharacter.sql
@@ -1,5 +1,5 @@
 DROP PROCEDURE IF EXISTS `sp_GetLoginCharacter`;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetLoginCharacter`(IN id BIGINT(20),IN account_id BIGINT(20))
+CREATE PROCEDURE `sp_GetLoginCharacter`(IN id BIGINT(20),IN account_id BIGINT(20))
 BEGIN
 
 	SELECT A.entity_id, B.baseModel, B.gender, B.species, C.x, C.y, C.z,
