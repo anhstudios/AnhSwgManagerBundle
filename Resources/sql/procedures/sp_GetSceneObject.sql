f28dde4b53499bf23ecf4569210853e2defb3c5b
Eric Barr <eb@anhstudios.com>
2011-09-04 20:44:54 -0400

    Fixed query selection

--- Resources/sql/procedures/sp_GetSceneObject.sql
+++ Resources/sql/procedures/sp_GetSceneObject.sql
@@ -6,11 +6,11 @@ BEGIN
 
     SELECT s.object_id, s.scene_id, s.template, s.stf_name_file,
            s.stf_name_string, s.custom_name, s.volume, s.x_position, 
-           s.y_position, s.z_position, s.x_orientation, s.x_orientation, 
-           s.x_orientation, s.x_orientation
+           s.y_position, s.z_position, s.x_orientation, s.y_orientation, 
+           s.z_orientation, s.w_orientation
            
     FROM scene_object s
-    
+        
     WHERE s.object_id = object_id;
 
 END
